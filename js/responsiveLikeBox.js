652b3fa74a5e98bd10868cdb773d6680db6e859f
Jacob Rudenstam <jrudenstam@gmail.com>
2012-06-13 23:54:16 +0200

    Made it all work with width and height. Code looks like shit

--- js/responsiveLikeBox.js
+++ js/responsiveLikeBox.js
@@ -1,18 +1,73 @@
-var widget = {
-	iframe : {
-		style : '',
-		resize : function (widget) {
-		},
-		$domElem : $('.fb-like-box iframe')
-	},
-	wrapper : {
-		style : JSON.parse(window.getComputedStyle(this.$domElem, '::after').content),
-		$domElem : $('.fb-like-box')
-	}
-}
+(function($) {
 
-window.addEventListner('resize', function () {
-	if(widget.iframe.style != widget.wrapper.style){
-		widget.iframe.resize(widget);
+	$.fn.responsiveLikeBox = function (options) {
+		
+		// Create some defaults, extending them with any options that were provided
+		var settings = $.extend( {
+			width : this.attr('data-width'),
+			height : this.attr('data-width'),
+			showFaces : this.attr('data-show-faces'),
+			showStream : this.attr('data-stream'),
+			showHeader : this.attr('data-header')
+		}, options);
+	
+		return this.each(function(){
+			// Create the widget object
+			var widget = this;
+			
+			// Extend to full object
+			widget.init = function () {
+				return $(this).each(function(){
+			    	$(window).bind('load.responsiveLikeBox resize.responsiveLikeBox', widget.iframe.resize);
+			    	//setTimeout(widget.iframe.resize, 5000); /* This must be iframe load event because iframe does'nt get src until! */
+			    });
+			};
+			
+			widget.wrapper = {
+			    $domElem : $(this),
+			    apperance : function(el) {
+			    	var data = window.getComputedStyle(el, '::after').content.replace('\'', '').replace('\'', ''); /* Hacky replace of quotes */
+			    	//console.log(data);
+			    	return JSON.parse(data)
+			    }
+			};
+			
+			widget.iframe = {
+			    apperance : settings, /* Should take from HTML implementation */
+			    resize : function () {
+			    	// Check if styles matches
+			    	var newApperance = widget.wrapper.apperance(widget);
+			    	
+			    	/* Check wether the src is set yet. Resize depends on it */
+			    	if (widget.wrapper.$domElem.find('iframe').attr('src')) {
+				    	if(JSON.stringify(widget.iframe.apperance) != JSON.stringify(newApperance)){
+				        	//Change all data in iframe and data-attrs
+				        	var newSrc = widget.wrapper.$domElem.find('iframe').attr('src').replace(/&width=([0-9]*)/g, '&width=' + newApperance.width).replace(/&height=([0-9]*)/g, '&height=' + newApperance.height);
+				        	
+				        	widget.wrapper.$domElem.attr('data-width', newApperance.width);
+				        	widget.wrapper.$domElem.attr('data-height', newApperance.height);
+				        	
+				        	widget.wrapper.$domElem.children('span').css('width', newApperance.width);
+				        	widget.wrapper.$domElem.children('span').css('height', newApperance.height);
+				        	
+				        	widget.wrapper.$domElem.find('iframe').css('width', newApperance.width);
+				        	widget.wrapper.$domElem.find('iframe').css('height', newApperance.height);
+				        	widget.wrapper.$domElem.find('iframe').attr('src', newSrc);
+				        	
+				        	// Set iframe apperance to the new apperance without actually checking. Should be a callback to iframe load.
+				        	widget.iframe.apperance = newApperance;
+				    	}	
+				    	
+			    	}
+			    	
+			    	else {
+				    	setTimeout(widget.iframe.resize, 1000); /* If src is'nt set yet try again */
+			    	}
+			    }
+			};
+			
+			widget.init();
+		});	
 	}
-})
+	
+})(jQuery);
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
-	$('.fb-like-box').responsiveFbWidget();
+$(function(){
+	$('.fb-like-box').responsiveLikeBox();
 });
