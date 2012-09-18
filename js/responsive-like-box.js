bbc55df563b521a3c18f819d102c6a69d05366d9
Jacob Rudenstam <jrudenstam@gmail.com>
2012-09-18 13:22:54 +0200

    Removed resizeend but added /*

--- js/responsive-like-box.js
+++ js/responsive-like-box.js
@@ -8,51 +8,6 @@ Released under MIT license
 http://jrudenstam.mit-license.org/
 */
 
-/* resizeend.js © 2012 Dominik Porada
- * Distributed under the MIT license: http://porada.mit-license.org
-••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••• */
-
-;(function(window, document) {
-  "use strict";
-
-  if ( !(window.addEventListener && document.createEvent && window.dispatchEvent) ) {
-    return;
-  }
-
-  var dispatchResizeEndEvent = function() {
-    var event = document.createEvent("Event");
-    event.initEvent("resizeend", false, false);
-    window.dispatchEvent(event);
-  };
-
-  // Assuming `window.orientation` is all about degrees
-  // (or nothing), the function returns either 0 or 90
-  var getCurrentOrientation = function() {
-    return Math.abs(+window.orientation || 0) % 180;
-  };
-
-  var initialOrientation = getCurrentOrientation();
-  var currentOrientation;
-  var resizeDebounceTimeout;
-
-  window.addEventListener("resize", function() {
-    currentOrientation = getCurrentOrientation();
-
-    // If `window` is resized due to an orientation change,
-    // dispatch `resizeend` immediately; otherwise, slightly delay it
-    if ( currentOrientation !== initialOrientation ) {
-      dispatchResizeEndEvent();
-      initialOrientation = currentOrientation;
-    }
-    else {
-      clearTimeout(resizeDebounceTimeout);
-      resizeDebounceTimeout = setTimeout(dispatchResizeEndEvent, 100);
-    }
-  }, false);
-
-})(window, document);
-
-// The plugin
 ;(function ($) {
 	'use strict';
 
@@ -113,7 +68,7 @@ http://jrudenstam.mit-license.org/
 				// Show loader on first load
 				loader = $('<img class="responsive-lb-loader" src="' + settings.loaderSrc + '" alt="Loading..."/>').appendTo(widget.wrapper.el.parent());
 				return $(this).each(function () {
-					$(window).bind('load.responsiveLikeBox resizeend.responsiveLikeBox', widget.iframe.resize);
+					$(window).bind('load.responsiveLikeBox resize.responsiveLikeBox orientationchange.responsiveLikeBox', widget.iframe.resize);
 				});
 			};
 
